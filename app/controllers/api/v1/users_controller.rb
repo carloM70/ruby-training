df7d4453f6d0014e2031e42a07fafa574f3cd322
carlo alarcon <carloalarconcoss@gmail.com>
2021-08-11 14:36:42 -0700

    using serializaer in create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render status: :created
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
